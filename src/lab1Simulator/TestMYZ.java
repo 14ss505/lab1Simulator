9b3fa5e8cb99614eaf5dd4e32b4f91076db860bb
XINGXIAOYU <14302010053@fudan.edu.cn>
2016-03-10 09:16:06 +0800

    little change

--- src/lab1Simulator/TestMYZ.java
+++ src/lab1Simulator/TestMYZ.java
@@ -2,7 +2,9 @@ package lab1Simulator;
 
 public class TestMYZ {
 	private String XINGXIAOYU;
+	public static String MAYEZHOU = "lovely";
 	public static void main(String[] args) {
 		System.out.println("he he...");
+		System.out.println(MAYEZHOU);
 	}
 }
